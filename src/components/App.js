d59c25ffc6787c086dd7096afb0140aa48eee8bb
Linn Johansen <linn.es.johansen@gmail.com>
2024-11-20 20:43:56 -0800

    Reset state

--- src/components/App.js
+++ src/components/App.js
@@ -61,6 +61,13 @@ function reducer(state, action) {
           state.points > state.highscore ? state.points : state.highscore,
       };
 
+    case 'restart':
+      return {
+        ...state,
+        questions: state.questions,
+        status: 'ready',
+      };
+
     default:
       throw new Error('Action unknown');
   }
@@ -120,6 +127,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
