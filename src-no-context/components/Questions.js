b41e399a4a79a6e0a8e72945f46626819e2222f0
Linn Johansen <linn.es.johansen@gmail.com>
2024-12-05 16:05:51 -0800

    Refactored to use the Context API

--- src-no-context/components/Questions.js
+++ src-no-context/components/Questions.js
@@ -1,14 +1,12 @@
-import { useQuiz } from '../contexts/QuizContext';
 import Options from './Options';
 
-function Questions() {
-  const { questions, index } = useQuiz();
+function Questions({ questions, index, dispatch, answer }) {
   const question = questions[index];
 
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} />
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
